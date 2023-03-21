7750e02616d143f5e6aa8168c689c4ff132b35e5
吴奇灵 <2473605320@qq.com>
2023-03-21 23:08:33 +0800

    重构代码,添加搜索

---
+++ src/js/index.js
@@ -0,0 +1,30 @@
+var mySwiper = new Swiper('.swiper', {
+    loop: true, // 无限循环属性
+
+    // 分页器
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    },
+
+    //自动切换
+    autoplay: {
+        delay: 3000,
+        stopOnLastSlide: false,
+        disableOnInteraction: true,
+    },
+})
+function showSearch() {
+    let searchBut = document.getElementById('searchBut'),
+        searchText = document.getElementById('searchText');
+    if (searchText.style.opacity == 0) {
+        searchText.style.opacity = 1;
+        searchText.style.width = '240px';
+    } else {
+        searchText.style.opacity = 0;
+        searchText.style.width = '0px';
+    }
+    $("#searchBut").click(function () {
+        $("#searchForm").submit();
+    });
+}
