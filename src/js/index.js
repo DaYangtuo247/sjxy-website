798ef275ef8da60965bd5d4601077c44a32ba719
吴奇灵 <2473605320@qq.com>
2023-03-24 22:36:49 +0800

    修改多处bug，以及添加诸多样式

--- src/js/index.js
+++ src/js/index.js
@@ -1,31 +1,33 @@
-$(function() {
-    new Swiper('.swiper', {
-        loop: true, // 无限循环属性
-        // 分页器
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true
-        },
+$(function () {
+	new Swiper(".swiper", {
+		loop: true, // 无限循环属性
+		// 分页器
+		pagination: {
+			el: ".swiper-pagination",
+			clickable: true,
+		},
 
-        //自动切换
-        autoplay: {
-            delay: 3000,
-            stopOnLastSlide: false,
-            disableOnInteraction: true,
-        },
-    })
-})
+		//自动切换
+		autoplay: {
+			delay: 3000,
+			stopOnLastSlide: false,
+			disableOnInteraction: true,
+		},
+	});
+});
 
 function showSearch() {
-    searchText = document.getElementById('searchText');
-    if (searchText.style.opacity == 0) {
-        searchText.style.opacity = 1;
-        searchText.style.width = '200px';
-    } else {
-        searchText.style.opacity = 0;
-        searchText.style.width = '0px';
-    }
-    $("#searchBut").click(function() {
-        $("#searchForm").submit();
-    });
+	var content = $("#searchText").val();
+	if (content && content.trim()) {
+		$("#searchBut").click(function () {
+			$("#searchForm").submit();
+		});
+	} else {
+		var searchText = $("#searchText");
+		if (searchText.css("opacity") == 0) {
+			searchText.css({ opacity: 1, width: "200px" });
+		} else {
+			searchText.css({ opacity: 0, width: "0px" });
+		}
+	}
 }
