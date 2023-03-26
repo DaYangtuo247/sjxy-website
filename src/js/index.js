5123394431646f3186aa13f0775cd4486749542b
吴奇灵 <2473605320@qq.com>
2023-03-26 19:42:43 +0800

    修复了searchBtm的bug

--- src/js/index.js
+++ src/js/index.js
@@ -102,21 +102,21 @@ window.addEventListener("load", function () {
 });
 
 // 显示/隐藏搜索框并提交
-function showSearch() {
-	var content = $("#searchText").val();
-	if (content && content.trim()) {
-		$("#searchBut").click(function () {
+$(document).ready(function () {
+	$("#searchBut").on("mousedown", function () {
+		var content = $("#searchText").val();
+		if (content && content.trim()) {
 			$("#searchForm").submit();
-		});
-	} else {
-		var searchText = $("#searchText");
-		if (searchText.css("opacity") == 0) {
-			searchText.css({ opacity: 1, width: "200px" });
 		} else {
-			searchText.css({ opacity: 0, width: "0px" });
+			var searchText = $("#searchText");
+			if (searchText.css("opacity") == 0) {
+				searchText.css({ opacity: 1, width: "200px" });
+			} else {
+				searchText.css({ opacity: 0, width: "0px" });
+			}
 		}
-	}
-}
+	});
+});
 
 function isIE() {
 	if (!!window.ActiveXObject || "ActiveXObject" in window) {
