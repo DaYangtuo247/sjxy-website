42c77a48e29bbff9be6ad9564a00891e191f7307
liljeong <3114729830@qq.com>
2023-03-29 18:42:57 +0800

    修改了分页器样式1.0

--- src/js/index.js
+++ src/js/index.js
@@ -1,104 +1,41 @@
 // 轮播图组件
-window.addEventListener("load", function () {
-	lock = false;
-	bgColor = [
-		"rgb(179, 189, 196)",
-		"rgb(180, 183, 166)",
-		"rgb(140, 152, 187)",
-	]; //背景色
-	var mySwiper = new Swiper(".swiper-container", {
-		speed: 1000,
-		allowTouchMove: true, //触摸滑动
-		// parallax: true, //文字位移视差
-		autoplay: true, //是否自动播放轮播图
-		// 如果需要分页器
-		pagination: {
-			el: ".swiper-pagination",
-			clickable: true,
-		},
-		on: {
-			transitionStart: function () {
-				lock = true; //锁住按钮
-				slides = this.slides;
-				imgBox = slides.eq(this.previousIndex).find(".img-box"); //图片包装器
-				imgPrev = slides.eq(this.previousIndex).find("img"); //当前图片
-				imgActive = slides.eq(this.activeIndex).find("img"); //下一张图片
-				derection = this.activeIndex - this.previousIndex;
-				this.$el.css("background-color", bgColor[this.activeIndex]); //背景颜色动画
+	// lock = false;
+	// var swiper = new Swiper(".mySwiper", {
+	// 	spaceBetween: 30,
+	// 	centeredSlides: true,
+	// 	speed: 1000,
+	// 	allowTouchMove: true, //触摸滑动
+	// 	autoplay: true, //是否自动播放轮播图
+	// 	pagination:'swiper-pagination',//分页容器的css选择器
+	// 	paginationType:'custom',//自定义-分页器样式类型
 
-				imgBox.transform("matrix(0.6, 0, 0, 0.6, 0, 0)");
-				imgPrev
-					.transition(1000)
-					.transform("matrix(1.2, 0, 0, 1.2, 0, 0)"); //图片缩放视差
-				this.slides
-					.eq(this.previousIndex)
-					.find("h3")
-					.transition(1000)
-					.css("color", "rgba(255,255,255,0)"); //文字透明动画
+	// 	pagination: {
+	// 		el: ".swiper-pagination",
+	// 		clickable: true,
+	// 		obeserver:true,
+	// 		observeParents:true,
+	// 		type:'custom',
+	// 	    renderBullet: function (index, pagination) {
+	// 			switch(index){
+	// 			  case 0:text='1';break;
+	// 			  case 1:text='2';break;
+	// 			  case 2:text='3';break;
+	// 			  case 3:text='4';break;
+	// 			  case 4:text='5';break;
+	// 			}
+	// 			return '<span class="' + pagination + '">' + text + '</span>';
+	// 		  },
+	// 	},
+	// 	navigation: {
+	// 		nextEl: ".swiper-button-next",
+	// 		prevEl: ".swiper-button-prev",
+	// 	},
+	// });
 
-				imgPrev.transitionEnd(function () {
-					imgActive
-						.transition(1300)
-						.transform(
-							"translate3d(0, 0, 0) matrix(1.2, 0, 0, 1.2, 0, 0)"
-						); //图片位移视差
-					imgPrev
-						.transition(1300)
-						.transform(
-							"translate3d(" +
-								60 * derection +
-								"%, 0, 0)  matrix(1.2, 0, 0, 1.2, 0, 0)"
-						);
-				});
-			},
-			transitionEnd: function () {
-				this.slides
-					.eq(this.activeIndex)
-					.find(".img-box")
-					.transform(" matrix(1, 0, 0, 1, 0, 0)");
-				imgActive = this.slides.eq(this.activeIndex).find("img");
-				imgActive
-					.transition(1000)
-					.transform(" matrix(1, 0, 0, 1, 0, 0)");
-				this.slides
-					.eq(this.activeIndex)
-					.find("h3")
-					.transition(1000)
-					.css("color", "rgba(255,255,255,1)");
-
-				imgActive.transitionEnd(function () {
-					lock = false;
-				});
-				//第一个和最后一个，禁止按钮
-				if (this.activeIndex == 0) {
-					this.$el.find(".button-prev").addClass("disabled");
-				} else {
-					this.$el.find(".button-prev").removeClass("disabled");
-				}
-
-				if (this.activeIndex == this.slides.length - 1) {
-					this.$el.find(".button-next").addClass("disabled");
-				} else {
-					this.$el.find(".button-next").removeClass("disabled");
-				}
-			},
-			init: function () {
-				this.emit("transitionEnd"); //在初始化时触发一次transitionEnd事件
-			},
-		},
-	});
-
-	//不使用自带的按钮组件，使用lock控制按钮锁定时间
-	mySwiper.$el.find(".button-next").on("click", function () {
-		if (!lock) {
-			mySwiper.slideNext();
-		}
-	});
-	mySwiper.$el.find(".button-prev").on("click", function () {
-		if (!lock) {
-			mySwiper.slidePrev();
-		}
-	});
+//给每个页码绑定跳转事件	
+$('.swiper-pagination').on('click','li',function(){
+	let index = this.innerHTML;
+	mySwiper.slideTo(index-1,500,false);
 });
 
 // 显示/隐藏搜索框并提交
@@ -155,31 +92,8 @@ $(function () {
 	//   $(".main-visual-slider .overlay").remove();
 	// }
 
-	var banner = new Swiper(".slide-banner", {
-		speed: 1500,
-		loop: true,
-		parallax: true,
-		lazy: {
-			loadPrevNext: true,
-		},
-		watchSlidesProgress: true,
-		spaceBetween: 0,
-		effect: "fade",
-		fadeEffect: { crossFade: true },
-		autoplay: {
-			delay: 6000,
-			stopOnLastSlide: false,
-			disableOnInteraction: false,
-		},
-		pagination: {
-			el: ".banner .pgba",
-			clickable: !0,
-			bulletActiveClass: "active",
-			renderBullet: function (index, className) {
-				return '<span class="' + className + '"><i></i></span>';
-			},
-		},
-		navigation: { nextEl: ".banner .next", prevEl: ".banner .prev" },
+	var swiper = new Swiper(".mySwiper", {
+		
 		on: {
 			transitionStart: function () {
 				// if (this.realIndex == 3) {delay = 20000;} else {
