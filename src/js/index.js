922bfdd0224c028e1a6b60602c75b723324201d4
吴奇灵 <2473605320@qq.com>
2023-04-02 13:24:09 +0800

    添加格式化插件，合并cj代码

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 // 大轮播图组件
-$(document).ready(function() {
+$(document).ready(function () {
     var swiper = new Swiper(".banner-swiper", {
         loop: true,
         speed: 1200,
@@ -12,22 +12,22 @@ $(document).ready(function() {
         },
         navigation: {
             prevEl: ".swiper-button-prev",
-            nextEl: ".swiper-button-next"
+            nextEl: ".swiper-button-next",
         },
         pagination: {
             el: ".swiper-pagination",
             clickable: true,
             bulletActiveClass: "active",
-            renderBullet: function(index, className) {
+            renderBullet: function (index, className) {
                 return '<span class="' + className + '">' + "<i></i></span>";
             },
         },
         //进度条配置
         on: {
-            init: function() {
+            init: function () {
                 this.emit("transitionStart");
             },
-            transitionStart: function() {
+            transitionStart: function () {
                 delay = this.params.autoplay.delay;
                 anime({
                     targets: ".banner-swiper .swiper-pagination .active i",
@@ -37,9 +37,8 @@ $(document).ready(function() {
                     duration: delay,
                 });
             },
-            slideChangeTransitionStart: function() {
-                var mainVisualImage =
-                    this.slides[this.activeIndex].querySelector("img,video");
+            slideChangeTransitionStart: function () {
+                var mainVisualImage = this.slides[this.activeIndex].querySelector("img,video");
                 anime({
                     targets: mainVisualImage,
                     scale: [1.1, 1],
@@ -67,10 +66,9 @@ $(document).ready(function() {
     });
 });
 
-
 // 显示/隐藏搜索框并提交
-$(document).ready(function() {
-    $("#searchBut").on("mousedown", function() {
+$(document).ready(function () {
+    $("#searchBut").on("mousedown", function () {
         var content = $("#searchText").val();
         if (content && content.trim()) {
             $("#searchForm").submit();
@@ -86,7 +84,7 @@ $(document).ready(function() {
 });
 
 //元素淡入淡出效果
-$(window).scroll(function() {
+$(window).scroll(function () {
     var scroll = $(window).scrollTop();
     var head_hight = $(".show").outerHeight();
     if (scroll >= head_hight) {
@@ -101,36 +99,36 @@ $(window).scroll(function() {
     if (scrollTop > (homeaTop + homecTop) / 2) {
         $(".fadeInUpa").addClass("animate__animated animate__fadeInUp");
         $(".fadeInUpRighta").addClass("animate__animated animate__fadeInLeft");
-        $(".fadeInUpLefta").each(function(i) {
+        $(".fadeInUpLefta").each(function (i) {
             $(this)
                 .delay(i * 100)
-                .queue(function() {
+                .queue(function () {
                     $(this).addClass("animate__animated animate__fadeInRight");
                 });
         });
     }
     if (scrollTop > (homeaTop + homebTop + homecTop) / 1.4) {
         $(".fadeInUpb").addClass("animate__animated animate__fadeInUp");
-        $(".fadeInUpbb").each(function(i) {
+        $(".fadeInUpbb").each(function (i) {
             $(this)
                 .delay(i * 100)
-                .queue(function() {
+                .queue(function () {
                     $(this).addClass("animate__animated animate__fadeInUp");
                 });
         });
-        $(".fadeInUpbbb").each(function(i) {
+        $(".fadeInUpbbb").each(function (i) {
             $(this)
                 .delay(i * 100)
-                .queue(function() {
+                .queue(function () {
                     $(this).addClass("animate__animated animate__fadeInUp");
                 });
         });
     }
     if (scrollTop > homecTop / 4) {
-        $(".fadeInUpc").each(function(i) {
+        $(".fadeInUpc").each(function (i) {
             $(this)
                 .delay(i * 100)
-                .queue(function() {
+                .queue(function () {
                     $(this).addClass("animate__animated animate__fadeInUp");
                 });
         });
@@ -138,34 +136,25 @@ $(window).scroll(function() {
 });
 
 // 通知公告添加new标签
-$(document).ready(function() {
+$(document).ready(function () {
     var inform = $(".flex .fadeInUpc").toArray();
     $.getJSON(
         "https://worldtimeapi.org/api/timezone/Asia/Shanghai", // 世界时间api接口
-        function(data) {
+        function (data) {
             var datetime = data.datetime;
             var ChinaDate = new Date(String(datetime.slice(0, 10))); // 提取年月日
             console.log("ChinaDate:", ChinaDate);
             for (var i = 0; i < inform.length; i++) {
                 var timeTag = $(inform[i]).find(".time");
-                var nowdate =
-                    timeTag.find("em").text() +
-                    "-" +
-                    timeTag.find("span").text();
+                var nowdate = timeTag.find("em").text() + "-" + timeTag.find("span").text();
                 nowdate = new Date(String(nowdate));
                 console.log("nowdate:", nowdate);
 
-                var diffInDays = Math.round(
-                    (ChinaDate - nowdate) / (1000 * 60 * 60 * 24)
-                );
+                var diffInDays = Math.round((ChinaDate - nowdate) / (1000 * 60 * 60 * 24));
                 console.log("diffInDays:", diffInDays);
 
                 if (diffInDays >= 0 && diffInDays <= 3) {
-                    $(inform[i])
-                        .find("a")
-                        .append(
-                            "<div class='new'><img src='./img/New.png'></div>"
-                        );
+                    $(inform[i]).find("a").append("<div class='new'><img src='./img/New.png'></div>");
                 }
             }
         }
